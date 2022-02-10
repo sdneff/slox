60bc95d232c6582e5eaf7395f9bccb1aab4ad43d
Scott Neff <sdneff@gmail.com>
2022-02-09 22:43:14 -0500

    make Token a record

--- Interpreter/Slox/Scanning/Token.cs
+++ Interpreter/Slox/Scanning/Token.cs
@@ -1,19 +1,3 @@
 namespace Slox.Scanning;
 
-public class Token
-{
-    public readonly TokenType Type;
-    public readonly string Lexeme;
-    public readonly object Literal;
-    public readonly int Line;
-
-    public Token(TokenType type, string lexeme, object literal, int line)
-    {
-        Type = type;
-        Lexeme = lexeme;
-        Literal = literal;
-        Line = line;
-    }
-
-    public override string ToString() => $"{Type} {Lexeme} {Literal}";
-}
+public record Token(TokenType Type, string Lexeme, object Literal, int Line);
